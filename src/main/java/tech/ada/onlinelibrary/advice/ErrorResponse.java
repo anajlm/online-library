a8ce758f992774e88c11e35e1846266b1bd4e86b
anajlm <anaj.limartins@gmail.com>
2024-02-19 08:17:50 -0300

    add: rest controller advice class

---
+++ src/main/java/tech/ada/onlinelibrary/advice/ErrorResponse.java
@@ -0,0 +1,33 @@
+package tech.ada.onlinelibrary.advice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private HttpStatus status;
+    private String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/tech/ada/onlinelibrary/advice/RestControllerAdvice.java
@@ -0,0 +1,19 @@
+package tech.ada.onlinelibrary.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandle(Exception exception){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
+
