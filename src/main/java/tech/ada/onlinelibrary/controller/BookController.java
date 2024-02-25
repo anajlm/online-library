4a910a923fe703645c9f9f2fb9eb85a3ba4f0c4f
anajlm <anaj.limartins@gmail.com>
2024-02-25 14:40:30 -0300

    Revert "solve conflicts"
    This reverts commit d04208262fe05534c24ff3a36fb1f4768d95bfed, reversing
    changes made to 97cc579d62162e2ce57b900c85b8696c9b1faa71.

--- src/main/java/tech/ada/onlinelibrary/controller/BookController.java
+++ src/main/java/tech/ada/onlinelibrary/controller/BookController.java
@@ -5,12 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.ada.onlinelibrary.domain.Book;
-<<<<<<< HEAD
 import tech.ada.onlinelibrary.dto.request.CreateBookRequest;
-=======
-import tech.ada.onlinelibrary.dto.BookPostRequest;
-import tech.ada.onlinelibrary.repository.BookRepository;
->>>>>>> andressa
 import tech.ada.onlinelibrary.service.BookService;
 
 import java.util.List;
@@ -35,28 +30,14 @@ public class BookController {
     //GET Retrieve book by genre.
 
     @PostMapping("/library/books")
-<<<<<<< HEAD
     public ResponseEntity<Book> createBook(@RequestBody CreateBookRequest bookRequest){
-=======
-    public ResponseEntity<Book> createBook(@RequestBody BookPostRequest bookRequest){
->>>>>>> andressa
         Book newbook = bookService.createBook(bookRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(newbook);
     }
 
     @DeleteMapping("/library/books/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
-<<<<<<< HEAD
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
-=======
-        Optional<Book> optionalBook = bookService.getBookById(id);
-        if (optionalBook.isPresent()) {
-            bookService.deleteBook(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
->>>>>>> andressa
     }
 }
