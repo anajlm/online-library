1af5fd897b64b8c600b95cf53e477ce5c2af0a31
anajlm <anaj.limartins@gmail.com>
2024-02-25 13:51:14 -0300

    add: rest controller advice

--- src/main/java/tech/ada/onlinelibrary/advice/exception/UserNotFoundException.java
+++ src/main/java/tech/ada/onlinelibrary/advice/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package tech.ada.onlinelibrary.advice.exception;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends NullPointerException {
 
     public UserNotFoundException(String username){ super("Could not find user with username " + username); }
 }
